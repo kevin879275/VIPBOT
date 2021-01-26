91fbb0131edf44b866fff25dd2617f9f17b813e2
kevinge <gevin879275@gmail.com>
2021-01-26 23:43:07 +0800

    UPDATE

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\vipqnamaker-bot-Web-Deploy.pubxml -p:Password=fNJ4RFymg6HsT3nRX6uSMtGrLLgzTK5vEGlHFfTk5DkWSCxQbkDLvsoo8c2H
+
