3332dfc1d1d1336fd607d2e610e616fa4b99e3c0
kevinge <gevin879275@gmail.com>
2021-01-27 00:03:19 +0800

    更新初版功能

---
+++ bin/Debug/netcoreapp3.1/IBotServices.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.AI.Luis;
+
+
+namespace Microsoft.BotBuilderSamples
+{
+    public interface IBotServices
+    {
+        LuisRecognizer Dispatch { get; }
+        QnAMaker SampleQnA { get; }
+    }
+}
