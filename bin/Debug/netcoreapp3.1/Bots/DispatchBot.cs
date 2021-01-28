4877db49bca59d1c3e50501f6756a2839864cbb1
kevinge <gevin879275@gmail.com>
2021-01-28 15:30:41 +0800

    update LUIS and QNAMAKER

--- bin/Debug/netcoreapp3.1/Bots/DispatchBot.cs
+++ bin/Debug/netcoreapp3.1/Bots/DispatchBot.cs
@@ -78,7 +78,7 @@ namespace Microsoft.BotBuilderSamples
             switch (intent)
             {
                 case "l_BuySell":
-                    var result = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(recognizerResult.Properties["luisResult"], new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }));
+                    //var result = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(recognizerResult.Properties["luisResult"], new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }));
                     //var topIntent = result.Prediction.Intents[result.Prediction.TopIntent];
                     await ProcessCovid19LuisAsync(turnContext, recognizerResult.Properties["luisResult"] as LuisResult, cancellationToken);
                     break;
@@ -89,146 +89,158 @@ namespace Microsoft.BotBuilderSamples
                     await ProcessSampleQnAAsync(turnContext, cancellationToken);
                     break;
                 default:
-                    _logger.LogInformation($"Covid19Bot unrecognized you.");
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Covid19Bot unrecognized you, kindly type a question as what is the symptoms of covid19."), cancellationToken);
+                    _logger.LogInformation($"機器人無法辨識您");
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"機器人無法辨識您"), cancellationToken);
                     break;
             }
         }
 
         private async Task ProcessCovid19LuisAsync(ITurnContext<IMessageActivity> turnContext, LuisResult luisResult, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("ProcessCovid19LuisAsync");
+            _logger.LogInformation("ProcessBuySellLuisAsync");
 
             // Retrieve LUIS result for Process Automation.
             var result = luisResult.ConnectedServiceResult;
             var topIntent = result.TopScoringIntent.Intent;
-            if (topIntent == "Covid19India")
+            if(topIntent == "Number")
             {
-                var url = $"https://api.covid19api.com/total/country/india";
-                var repositories = ProcessRepo(url);
-                List<string> Ystdata = new List<string>();
-                List<string> Tdydata = new List<string>();
 
-                if (repositories.Count > 0)
-                {
-                    var ystData = repositories[repositories.Count - 2];
-                    var todayData = repositories.LastOrDefault();
-
-                    DateTime Todaydatetime = DateTime.Parse(todayData.date);
-                    DateTime Ystdatetime = DateTime.Parse(ystData.date);
-
-                    var confirmedInc = todayData.confirmed - ystData.confirmed;
-                    var confirmedIncPCT = (((Math.Abs(Convert.ToDecimal(confirmedInc))) / (Math.Abs(Convert.ToDecimal(todayData.confirmed)))) * 100).ToString("0.00");
-
-                    var activeInc = todayData.active - ystData.active;
-                    var activeIncPCT = (((Math.Abs(Convert.ToDecimal(activeInc))) / (Math.Abs(Convert.ToDecimal(todayData.active)))) * 100).ToString("0.00"); //(activeInc / todayData.active) * 100;
-
-                    var recoveredInc = todayData.recovered - ystData.recovered;
-                    var recoveredIncPCT = (((Math.Abs(Convert.ToDecimal(recoveredInc))) / (Math.Abs(Convert.ToDecimal(todayData.recovered)))) * 100).ToString("0.00"); //(recoveredInc / todayData.recovered) * 100;
-
-                    var deceasedInc = todayData.deaths - ystData.deaths;
-                    var deceasedIncPCT = (((Math.Abs(Convert.ToDecimal(deceasedInc))) / (Math.Abs(Convert.ToDecimal(todayData.deaths)))) * 100).ToString("0.00"); //(deceasedInc / todayData.deaths) * 100;
-
-                    Ystdata.Add(ystData.confirmed.ToString());
-                    Ystdata.Add(ystData.deaths.ToString());
-                    Ystdata.Add(ystData.recovered.ToString());
-                    Ystdata.Add(ystData.active.ToString());
-                    Ystdata.Add(Ystdatetime.ToString("dd MMM yyyy"));
-
-                    Tdydata.Add(todayData.confirmed.ToString());
-                    Tdydata.Add(todayData.deaths.ToString());
-                    Tdydata.Add(todayData.recovered.ToString());
-                    Tdydata.Add(todayData.active.ToString());
-                    Tdydata.Add(Todaydatetime.ToString("dd MMM yyyy") + " " + "India");
-                    Tdydata.Add("▲ " + " " + confirmedInc + " " + "(" + confirmedIncPCT + " " + "%" + ")");
-                    Tdydata.Add("▲ " + " " + activeInc + " " + "(" + activeIncPCT + " " + "%" + ")");
-                    Tdydata.Add("▲ " + " " + recoveredInc + " " + "(" + recoveredIncPCT + " " + "%" + ")");
-                    Tdydata.Add("▲ " + " " + deceasedInc + " " + "(" + deceasedIncPCT + " " + "%" + ")");
-                }
-
-                var Covid19StatusCardRead = readFileforUpdate_jobj(_cards[0]);
-
-                JToken Date = Covid19StatusCardRead.SelectToken("body[0].items[2].text");
-                JToken ConfirmedInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[0].items[1].text");
-                JToken Confirmed = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[0].items[2].text");
-
-                JToken ActiveInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[1].items[1].text");
-                JToken Active = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[1].items[2].text");
-
-                JToken RecoveredInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[2].items[1].text");
-                JToken Recovered = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[2].items[2].text");
-
-                JToken DeceasedInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[3].items[1].text");
-                JToken Deceased = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[3].items[2].text");
-
-                Date.Replace(Tdydata[4]);
-                Confirmed.Replace(Tdydata[0]);
-                Active.Replace(Tdydata[3]);
-                Recovered.Replace(Tdydata[2]);
-                Deceased.Replace(Tdydata[1]);
-
-                ConfirmedInc.Replace(Tdydata[5]);
-                ActiveInc.Replace(Tdydata[6]);
-                RecoveredInc.Replace(Tdydata[7]);
-                DeceasedInc.Replace(Tdydata[8]);
-
-                var Covid19StatusCardFinal = UpdateAdaptivecardAttachment(Covid19StatusCardRead);
-                var response = MessageFactory.Attachment(Covid19StatusCardFinal, ssml: "Covid19 Live status card!");
-                await turnContext.SendActivityAsync(response, cancellationToken);
             }
-            else
-            if (topIntent == "WorldCovid19")
+            else if(topIntent == "Buy")
             {
-                var url = $"https://api.covid19api.com/summary";
-                var repositories = ProcessRepoWorldJ(url);
-                List<string> Tdydata = new List<string>();
 
-                var TotalConfirmed = (string)repositories.SelectToken("Global.TotalConfirmed");
-                var NewConfirmed = (string)repositories.SelectToken("Global.NewConfirmed");
-                var TotalDeaths = (string)repositories.SelectToken("Global.TotalDeaths");
-                var NewDeaths = (string)repositories.SelectToken("Global.NewDeaths");
-                var TotalRecovered = (string)repositories.SelectToken("Global.TotalRecovered");
-                var NewRecovered = (string)repositories.SelectToken("Global.NewRecovered");
-                var date = (DateTime)repositories.SelectToken("Date");
-                //DateTime datetime1 = DateTime.Parse(date);
-                //DateTime datetime1 = DateTime.Parse(date);
-                var Todaydatetime = date.ToString("dd MMM yyyy") + " " + "Global";
-                var confirmedIncPCT = (((Math.Abs(Convert.ToDecimal(NewConfirmed))) / (Math.Abs(Convert.ToDecimal(TotalConfirmed)))) * 100).ToString("0.00");
-                var recoveredIncPCT = (((Math.Abs(Convert.ToDecimal(NewRecovered))) / (Math.Abs(Convert.ToDecimal(TotalRecovered)))) * 100).ToString("0.00");
-                var deathsIncPCT = (((Math.Abs(Convert.ToDecimal(NewDeaths))) / (Math.Abs(Convert.ToDecimal(TotalDeaths)))) * 100).ToString("0.00");
-
-                var confirmIncFinal = "▲ " + " " + NewConfirmed + " " + "(" + confirmedIncPCT + " " + "%" + ")";
-                var recoveredFinal = "▲ " + " " + NewRecovered + " " + "(" + recoveredIncPCT + " " + "%" + ")";
-                var deathsIncFinal = "▲ " + " " + NewDeaths + " " + "(" + deathsIncPCT + " " + "%" + ")";
-
-                var GlobalStatusCardRead = readFileforUpdate_jobj(_cards[1]);
-
-                JToken Date = GlobalStatusCardRead.SelectToken("body[0].items[2].text");
-                JToken ConfirmedInc = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[0].items[1].text");
-                JToken Confirmed = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[0].items[2].text");
-
-                JToken RecoveredInc = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[1].items[1].text");
-                JToken Recovered = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[1].items[2].text");
-
-                JToken DeceasedInc = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[2].items[1].text");
-                JToken Deceased = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[2].items[2].text");
-
-                Date.Replace(Todaydatetime);
-                ConfirmedInc.Replace(confirmIncFinal);
-                Confirmed.Replace(TotalConfirmed);
-                RecoveredInc.Replace(recoveredFinal);
-                Recovered.Replace(TotalRecovered);
-                DeceasedInc.Replace(deathsIncFinal);
-                Deceased.Replace(TotalDeaths);
-                //var GlobalStatusCardFinal = CreateAdaptiveCardAttachment(_cards[1]);
-                var GlobalStatusCardFinal = UpdateAdaptivecardAttachment(GlobalStatusCardRead);
-                var response = MessageFactory.Attachment(GlobalStatusCardFinal, ssml: "Global Live status card!");
-                await turnContext.SendActivityAsync(response, cancellationToken);
             }
+            else if(topIntent == "Sell")
+            {
+
+            }
+            //if (topIntent == "Covid19India")
+            //{
+            //    var url = $"https://api.covid19api.com/total/country/india";
+            //    var repositories = ProcessRepo(url);
+            //    List<string> Ystdata = new List<string>();
+            //    List<string> Tdydata = new List<string>();
+
+            //    if (repositories.Count > 0)
+            //    {
+            //        var ystData = repositories[repositories.Count - 2];
+            //        var todayData = repositories.LastOrDefault();
+
+            //        DateTime Todaydatetime = DateTime.Parse(todayData.date);
+            //        DateTime Ystdatetime = DateTime.Parse(ystData.date);
+
+            //        var confirmedInc = todayData.confirmed - ystData.confirmed;
+            //        var confirmedIncPCT = (((Math.Abs(Convert.ToDecimal(confirmedInc))) / (Math.Abs(Convert.ToDecimal(todayData.confirmed)))) * 100).ToString("0.00");
+
+            //        var activeInc = todayData.active - ystData.active;
+            //        var activeIncPCT = (((Math.Abs(Convert.ToDecimal(activeInc))) / (Math.Abs(Convert.ToDecimal(todayData.active)))) * 100).ToString("0.00"); //(activeInc / todayData.active) * 100;
+
+            //        var recoveredInc = todayData.recovered - ystData.recovered;
+            //        var recoveredIncPCT = (((Math.Abs(Convert.ToDecimal(recoveredInc))) / (Math.Abs(Convert.ToDecimal(todayData.recovered)))) * 100).ToString("0.00"); //(recoveredInc / todayData.recovered) * 100;
+
+            //        var deceasedInc = todayData.deaths - ystData.deaths;
+            //        var deceasedIncPCT = (((Math.Abs(Convert.ToDecimal(deceasedInc))) / (Math.Abs(Convert.ToDecimal(todayData.deaths)))) * 100).ToString("0.00"); //(deceasedInc / todayData.deaths) * 100;
+
+            //        Ystdata.Add(ystData.confirmed.ToString());
+            //        Ystdata.Add(ystData.deaths.ToString());
+            //        Ystdata.Add(ystData.recovered.ToString());
+            //        Ystdata.Add(ystData.active.ToString());
+            //        Ystdata.Add(Ystdatetime.ToString("dd MMM yyyy"));
+
+            //        Tdydata.Add(todayData.confirmed.ToString());
+            //        Tdydata.Add(todayData.deaths.ToString());
+            //        Tdydata.Add(todayData.recovered.ToString());
+            //        Tdydata.Add(todayData.active.ToString());
+            //        Tdydata.Add(Todaydatetime.ToString("dd MMM yyyy") + " " + "India");
+            //        Tdydata.Add("▲ " + " " + confirmedInc + " " + "(" + confirmedIncPCT + " " + "%" + ")");
+            //        Tdydata.Add("▲ " + " " + activeInc + " " + "(" + activeIncPCT + " " + "%" + ")");
+            //        Tdydata.Add("▲ " + " " + recoveredInc + " " + "(" + recoveredIncPCT + " " + "%" + ")");
+            //        Tdydata.Add("▲ " + " " + deceasedInc + " " + "(" + deceasedIncPCT + " " + "%" + ")");
+            //    }
+
+            //    var Covid19StatusCardRead = readFileforUpdate_jobj(_cards[0]);
+
+            //    JToken Date = Covid19StatusCardRead.SelectToken("body[0].items[2].text");
+            //    JToken ConfirmedInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[0].items[1].text");
+            //    JToken Confirmed = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[0].items[2].text");
+
+            //    JToken ActiveInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[1].items[1].text");
+            //    JToken Active = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[1].items[2].text");
+
+            //    JToken RecoveredInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[2].items[1].text");
+            //    JToken Recovered = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[2].items[2].text");
+
+            //    JToken DeceasedInc = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[3].items[1].text");
+            //    JToken Deceased = Covid19StatusCardRead.SelectToken("body[0].items[3].columns[3].items[2].text");
+
+            //    Date.Replace(Tdydata[4]);
+            //    Confirmed.Replace(Tdydata[0]);
+            //    Active.Replace(Tdydata[3]);
+            //    Recovered.Replace(Tdydata[2]);
+            //    Deceased.Replace(Tdydata[1]);
+
+            //    ConfirmedInc.Replace(Tdydata[5]);
+            //    ActiveInc.Replace(Tdydata[6]);
+            //    RecoveredInc.Replace(Tdydata[7]);
+            //    DeceasedInc.Replace(Tdydata[8]);
+
+            //    var Covid19StatusCardFinal = UpdateAdaptivecardAttachment(Covid19StatusCardRead);
+            //    var response = MessageFactory.Attachment(Covid19StatusCardFinal, ssml: "Covid19 Live status card!");
+            //    await turnContext.SendActivityAsync(response, cancellationToken);
+            //}
+            //else
+            //if (topIntent == "WorldCovid19")
+            //{
+            //    var url = $"https://api.covid19api.com/summary";
+            //    var repositories = ProcessRepoWorldJ(url);
+            //    List<string> Tdydata = new List<string>();
+
+            //    var TotalConfirmed = (string)repositories.SelectToken("Global.TotalConfirmed");
+            //    var NewConfirmed = (string)repositories.SelectToken("Global.NewConfirmed");
+            //    var TotalDeaths = (string)repositories.SelectToken("Global.TotalDeaths");
+            //    var NewDeaths = (string)repositories.SelectToken("Global.NewDeaths");
+            //    var TotalRecovered = (string)repositories.SelectToken("Global.TotalRecovered");
+            //    var NewRecovered = (string)repositories.SelectToken("Global.NewRecovered");
+            //    var date = (DateTime)repositories.SelectToken("Date");
+            //    //DateTime datetime1 = DateTime.Parse(date);
+            //    //DateTime datetime1 = DateTime.Parse(date);
+            //    var Todaydatetime = date.ToString("dd MMM yyyy") + " " + "Global";
+            //    var confirmedIncPCT = (((Math.Abs(Convert.ToDecimal(NewConfirmed))) / (Math.Abs(Convert.ToDecimal(TotalConfirmed)))) * 100).ToString("0.00");
+            //    var recoveredIncPCT = (((Math.Abs(Convert.ToDecimal(NewRecovered))) / (Math.Abs(Convert.ToDecimal(TotalRecovered)))) * 100).ToString("0.00");
+            //    var deathsIncPCT = (((Math.Abs(Convert.ToDecimal(NewDeaths))) / (Math.Abs(Convert.ToDecimal(TotalDeaths)))) * 100).ToString("0.00");
+
+            //    var confirmIncFinal = "▲ " + " " + NewConfirmed + " " + "(" + confirmedIncPCT + " " + "%" + ")";
+            //    var recoveredFinal = "▲ " + " " + NewRecovered + " " + "(" + recoveredIncPCT + " " + "%" + ")";
+            //    var deathsIncFinal = "▲ " + " " + NewDeaths + " " + "(" + deathsIncPCT + " " + "%" + ")";
+
+            //    var GlobalStatusCardRead = readFileforUpdate_jobj(_cards[1]);
+
+            //    JToken Date = GlobalStatusCardRead.SelectToken("body[0].items[2].text");
+            //    JToken ConfirmedInc = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[0].items[1].text");
+            //    JToken Confirmed = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[0].items[2].text");
+
+            //    JToken RecoveredInc = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[1].items[1].text");
+            //    JToken Recovered = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[1].items[2].text");
+
+            //    JToken DeceasedInc = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[2].items[1].text");
+            //    JToken Deceased = GlobalStatusCardRead.SelectToken("body[0].items[3].columns[2].items[2].text");
+
+            //    Date.Replace(Todaydatetime);
+            //    ConfirmedInc.Replace(confirmIncFinal);
+            //    Confirmed.Replace(TotalConfirmed);
+            //    RecoveredInc.Replace(recoveredFinal);
+            //    Recovered.Replace(TotalRecovered);
+            //    DeceasedInc.Replace(deathsIncFinal);
+            //    Deceased.Replace(TotalDeaths);
+            //    //var GlobalStatusCardFinal = CreateAdaptiveCardAttachment(_cards[1]);
+            //    var GlobalStatusCardFinal = UpdateAdaptivecardAttachment(GlobalStatusCardRead);
+            //    var response = MessageFactory.Attachment(GlobalStatusCardFinal, ssml: "Global Live status card!");
+            //    await turnContext.SendActivityAsync(response, cancellationToken);
+            //}
             else
             {
                 _logger.LogInformation($"Luis unrecognized intent.");
-                await turnContext.SendActivityAsync(MessageFactory.Text($"Covid19Bot unrecognized your inputs, kindly reply as 'live covid19 status'."), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text($"機器人無法辨識您的輸入!"), cancellationToken);
             }
         }
 
@@ -243,7 +255,7 @@ namespace Microsoft.BotBuilderSamples
             }
             else
             {
-                await turnContext.SendActivityAsync(MessageFactory.Text("Sorry, could not find an answer in the Q and A, kindly type a question as 'what is the symptoms of covid19'."), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text("抱歉! 機器人無法回答您的問題"), cancellationToken);
             }
         }
 
