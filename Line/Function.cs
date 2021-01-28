ba6b109c3c2f2fb7003faf3039a2d3475b527445
louisfghbvc <43661591+louisfghbvc@users.noreply.github.com>
2021-01-28 21:01:36 +0800

    add line method

---
+++ Line/Function.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples
+{
+    class Collections
+    {
+        public async Task LineMsg(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var channelData = turnContext.Activity.ChannelData;
+            var jsonObj = JsonConvert.SerializeObject(channelData);
+
+            string msgType = jsonObj["type"];
+            switch (msgType)
+            {
+                case "location":
+                    await turnContext.SendActivityAsync(MessageFactory.Text(jsonObj["address"]), cancellationToken);
+                    break;
+                case "text":
+                    break;
+                case "image":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+}
