9d35570fd21378052610cab210e53c342d78c271
louisfghbvc <43661591+louisfghbvc@users.noreply.github.com>
2021-01-29 19:19:07 +0800

    update model

--- Line/LineModel.cs
+++ Line/LineModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Microsoft.BotBuilderSamples
@@ -36,6 +37,65 @@ namespace Microsoft.BotBuilderSamples
         public string Type { get; set; }
         [JsonPropertyName("text")]
         public string TextString { get; set; }
+    }
+
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Hero
+    {
+        public string type { get; set; }
+        public string url { get; set; }
+        public string align { get; set; }
+        public string size { get; set; }
+        public string aspectRatio { get; set; }
+        public string aspectMode { get; set; }
+        public string backgroundColor { get; set; }
+    }
+
+    public class Content
+    {
+        public string type { get; set; }
+        public string text { get; set; }
+        public string weight { get; set; }
+        public string size { get; set; }
+        public bool wrap { get; set; }
+        public List<object> contents { get; set; }
+        public string align { get; set; }
+        public string gravity { get; set; }
+        public Action action { get; set; }
+        public string style { get; set; }
+    }
+
+    public class Body
+    {
+        public string type { get; set; }
+        public string layout { get; set; }
+        public string spacing { get; set; }
+        public List<Content> contents { get; set; }
+    }
+
+    public class Action
+    {
+        public string type { get; set; }
+        public string label { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Footer
+    {
+        public string type { get; set; }
+        public string layout { get; set; }
+        public string spacing { get; set; }
+        public List<Content> contents { get; set; }
+    }
+
+    public class Card
+    {
+        public string type { get; set; }
+        public string direction { get; set; }
+        public Hero hero { get; set; }
+        public Body body { get; set; }
+        public Footer footer { get; set; }
     }
 
 }
