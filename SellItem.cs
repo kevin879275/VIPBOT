caea628275d5c3a24632f5cbbc09d96a991693e9
NawaNae <t105590029@ntut.edu.tw>
2021-01-29 13:38:00 +0800

    SellItem

---
+++ SellItem.cs
@@ -0,0 +1,15 @@
+namespace Microsoft.BotBuilderSamples
+{
+
+  class SellItem
+  {
+    public string imageSrc;
+    public string type;//物品種類
+    public string time;
+    public string description;//name         
+    public string status;//selling / sold out
+    public string location;//JSON Style
+    public string ownerUserId;
+    public int price;
+  }
+}
