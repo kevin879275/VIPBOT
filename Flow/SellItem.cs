bb3a7e1f017ee3b5596994ad2136d4d260827153
NawaNae <t105590029@ntut.edu.tw>
2021-01-29 17:36:17 +0800

    sellItemQuantity

--- Flow/SellItem.cs
+++ Flow/SellItem.cs
@@ -1,15 +1,16 @@
 namespace Microsoft.BotBuilderSamples
 {
 
-  class SellItem
-  {
-    public string imageSrc { get; set; }
-    public string type { get; set; }//物品種類
-    public string time { get; set; }
-    public string description { get; set; }//name         
-    public string status { get; set; }//selling / sold out
-    public string location { get; set; }//JSON Style
-    public string ownerUserId { get; set; }
-    public int price { get; set; }
-  }
+    class SellItem
+    {
+        public string imageSrc { get; set; }
+        public string type { get; set; }//物品種類
+        public string time { get; set; }
+        public string description { get; set; }//name         
+        public string status { get; set; }//selling / sold out
+        public string location { get; set; }//JSON Style
+        public string ownerUserId { get; set; }
+        public int quantity { get; set; }
+        public int price { get; set; }
+    }
 }
